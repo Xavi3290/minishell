90082ffb469e46bf9d807be431a16704f7834d1b
Xavier Roca Pérez <xroca-pe@car11s4.42barcelona.com>
2024-08-22 16:45:52 +0200

    wildcards works better

--- src/expand/expand_wildcards.c
+++ src/expand/expand_wildcards.c
@@ -6,13 +6,13 @@
 /*   By: xroca-pe <xroca-pe@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/20 16:23:05 by xroca-pe          #+#    #+#             */
-/*   Updated: 2024/08/22 16:08:32 by xroca-pe         ###   ########.fr       */
+/*   Updated: 2024/08/22 16:40:01 by xroca-pe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minishell.h"
 
-static int	match_pattern(const char *pattern, const char *filename)
+/*static int	match_pattern(const char *pattern, const char *filename)
 {
 	while (*pattern && *filename)
 	{
@@ -31,9 +31,35 @@ static int	match_pattern(const char *pattern, const char *filename)
 			return (0);
 	}
 	return (*pattern == '\0' && *filename == '\0');
+}*/
+
+static int	match_pattern(const char *pattern, const char *filename)
+{
+	while (*pattern)
+	{
+		if (*pattern == '*')
+		{
+			while (*pattern == '*')
+				pattern++;
+			if (*pattern == '\0')
+				return (1);
+			while (*filename && !match_pattern(pattern, filename))
+				filename++;
+			if (*filename == '\0')
+				return (0);
+		}
+		else if (*pattern == *filename)
+		{
+			pattern++;
+			filename++;
+		}
+		else
+			return (0);
+	}
+	return (*filename == '\0');
 }
 
-static char	**read_directory(DIR *dir, const char *pattern)
+/*static char	**read_directory(DIR *dir, const char *pattern)
 {
 	struct dirent	*entry;
 	char			**matches;
@@ -44,6 +70,11 @@ static char	**read_directory(DIR *dir, const char *pattern)
 	entry = readdir(dir);
 	while (entry != NULL)
 	{
+		if (entry->d_name[0] == '.')
+		{
+			entry = readdir(dir);
+			continue;
+		}
 		if (match_pattern(pattern, entry->d_name))
 		{
 			matches = ft_realloc(matches, sizeof(char *) * (count + 1),
@@ -59,7 +90,43 @@ static char	**read_directory(DIR *dir, const char *pattern)
 	if (matches)
 		matches[count] = NULL;
 	return (matches);
+}*/
+
+static char	**get_matching_files(char **matches, int *count, const char *pattern, const char *filename)
+{
+	if (match_pattern(pattern, filename))
+	{
+		matches = ft_realloc(matches, sizeof(char *) * (*count + 1),
+				sizeof(char *) * (*count + 2));
+		if (!matches)
+			handle_error(NULL, NULL);
+		matches[*count] = ft_strdup(filename);
+		(*count)++;
+	}
+	return (matches);
 }
+
+static char	**read_directory(DIR *dir, const char *pattern)
+{
+	struct dirent	*entry;
+	char			**matches;
+	int				count;
+
+	matches = NULL;
+	count = 0;
+	entry = readdir(dir);
+	while (entry != NULL)
+	{
+		if (entry->d_name[0] != '.')
+			matches = get_matching_files(matches, &count, pattern, entry->d_name);
+		entry = readdir(dir);
+	}
+	closedir(dir);
+	if (matches)
+		matches[count] = NULL;
+	return (matches);
+}
+
 
 char	**expand_wildcards(const char *pattern, t_shell *shell)
 {
@@ -93,18 +160,4 @@ void	expand_wildcard_token(t_token **tokens, t_shell *shell,
 			*current = *tokens;
 	}
 	free_string_array(expansions);
-}
-
-void	join_token_values(t_token *current, t_token *next)
-{
-	char	*joined_value;
-
-	joined_value = ft_strjoin(current->value, next->value);
-	free(current->value);
-	current->value = joined_value;
-	current->next = next->next;
-	free(next->value);
-	free(next);
-	if (current->type == DELIMITER)
-		current->type = DOUBLE_QUOTES;
 }
---
+++ src/expand/token_join.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   expand_utils2.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: xroca-pe <xroca-pe@student.42barcel>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/22 16:38:08 by xroca-pe          #+#    #+#             */
+/*   Updated: 2024/08/22 16:38:37 by xroca-pe         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../includes/minishell.h"
+
+void	join_token_values(t_token *current, t_token *next)
+{
+	char	*joined_value;
+
+	joined_value = ft_strjoin(current->value, next->value);
+	free(current->value);
+	current->value = joined_value;
+	current->next = next->next;
+	free(next->value);
+	free(next);
+	if (current->type == DELIMITER)
+		current->type = DOUBLE_QUOTES;
+}
